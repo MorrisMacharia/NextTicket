f990d1dafff81cd4c51cbddf8a452a941ea5eac5
EssKare <esskaree@gmail.com>
2024-03-27 16:45:50 +0700

    updated ticketing code

---
+++ src/app/Event/page.js
@@ -0,0 +1,55 @@
+import React from "react";
+import "../../components/Styles/Event.css";
+import { MdLanguage } from "react-icons/md";
+import { IoSearchOutline } from "react-icons/io5";
+import { MdFavoriteBorder } from "react-icons/md";
+import { CiShare2 } from "react-icons/ci";
+
+const Event = () => {
+  return (
+    <div className="container11">
+      <div className="logo">
+        <img src="/Logo.svg" alt="" />
+      </div>
+      <div className="searchbar11">
+        <input type="text" className="cityinput11" />
+        <IoSearchOutline />
+      </div>
+      <div className="wordings11">
+        <div className="create"> Create Event</div>
+
+        <div className="create">
+          <MdLanguage /> English
+        </div>
+        <button type="button" className="btn">
+          Sign in
+        </button>
+      </div>
+      <div className="dets">
+        <div className="neutral">
+          <div>
+            <img src="/Event1.png" alt="card" />
+          </div>
+          <div className="details">
+            <div className="upper">
+              <div className="head1">
+                <div>Passionate Pages Flavoured by Manyatta Cider</div>
+                <div>
+                  <CiShare2 />
+                  <CiShare2 />
+                </div>
+              </div>
+              <div className="time">
+                <div>Sat, Mar 23</div>
+                <div>6:00pm EAT</div>
+                <div>New Muthaiga Mall, Thigiri Ridge</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Event;
--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,7 +1,48 @@
 import React from "react";
+import "../../components/Styles/login.css";
+import { FcGoogle } from "react-icons/fc";
+import { FaApple } from "react-icons/fa";
+import Link from "next/link";
 
 const Page = () => {
-  return <div>About Page</div>;
+  return (
+    <div className="log">
+      <div className="bar">
+        <div className="logo">
+          <img src="/Logo.svg" alt="logo" />
+        </div>
+        <div className="cancel">
+          <button>X</button>
+        </div>
+      </div>
+      <div className="bars">
+        <div className="wave">Welcome to TicketWave</div>
+        <div className="bttns1">
+          <div className="enter">
+            <div>Enter your Email</div>
+            <input type="email" placeholder="Email" className="email1" />
+          </div>
+          <Link href="/Password" target="blank">
+            <button type="button" className="btn3">
+              Continue
+            </button>
+          </Link>
+
+          <Link href="/Event">
+            <button type="button" className="btn4">
+              <FcGoogle /> Continue with Google
+            </button>
+          </Link>
+          <button type="button" className="btn4">
+            <FaApple /> Continue with Apple
+          </button>
+          <div className="sgn">
+            <a href="url">sign in</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
