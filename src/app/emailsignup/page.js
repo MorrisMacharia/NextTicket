159b99d01c4d99ebb3bce7eb548f65d456608da0
EssKare <esskaree@gmail.com>
2024-04-03 16:10:48 +0700

    updates

--- src/app/emailsignup/page.js
+++ src/app/emailsignup/page.js
@@ -22,11 +22,7 @@ const Emailsignup = () => {
           <input type="email" placeholder="Email " />
           <div className="e-add">Your password</div>
           <input type="password" placeholder="password " />
-          <button className="button3">
-            <Link href="/authenticate">
-            Sign in
-            </Link>
-            </button>
+          <button className="button3">Sign in</button>
         </div>
         <div className="pass4">
           <div className="send">Send a code via email instead</div>
